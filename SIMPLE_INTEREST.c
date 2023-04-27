3a9bb8d359ef5aa3cc31050abe2ba84a02491db7
ANKIT ROY CHOWDHURY <121609077+ANKITROYCHOWDHURY54@users.noreply.github.com>
2023-04-27 21:10:40 +0530

    #include<stdio.h> int main() {     int p,t,r;     scanf("%d%d%d",&p,&t,&r);     int si;     si = (p*t*r)/100;     printf("%d",si); }

--- SIMPLE_INTEREST.c
+++ SIMPLE_INTEREST.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
-void si(int p,int t,int r)
-{
-    int tc;
-    tc=(p*t*r)/100;
-    printf("%d",tc);
-}
 int main()
 {
     int p,t,r;
     scanf("%d%d%d",&p,&t,&r);
-    si(p,t,r);
+    int si;
+    si = (p*t*r)/100;
+    printf("%d",si);
 }
